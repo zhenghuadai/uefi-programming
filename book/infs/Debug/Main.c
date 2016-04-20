2ff8939e94b22f9a8e83dde510af5a2dad398dfc
Iru Cai <mytbk920423@gmail.com>
2016-04-20 10:12:46 +0800

    Fix asm int 3 in GCC

--- book/infs/Debug/Main.c
+++ book/infs/Debug/Main.c
@@ -6,7 +6,11 @@ UefiMain (
           IN EFI_SYSTEM_TABLE  *SystemTable
           )
 {
-   _asm int 3
+#ifdef _MSC_VER
+	_asm int 3;
+#else
+	asm("int $0x03");
+#endif
    SystemTable -> ConOut-> OutputString(SystemTable->ConOut, L"HelloWorld\n"); 
    return EFI_SUCCESS;
 }
